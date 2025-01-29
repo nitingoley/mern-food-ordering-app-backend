34fd73cd7846a292ce97b29a9d34044fcd6c3e59
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-01-29 23:57:50 +0530

    Update index.ts
    add

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_DB as string)
 const app = express();
 app.use(express.json());
 app.use(cors({
-   origin: "https://mern-food-ordering-app-frontend-zeta.vercel.app/",
+   origin: "*",
     credentials: true, 
 }));
 
